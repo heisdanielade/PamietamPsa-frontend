7dfc771757c51f775f893a17313dc62cd64f9315
heisdanielade <heisdanielade@gmail.com>
2025-01-09 14:28:32 +0100

    Update: minor changes

--- src/components/home/sections/Header.vue
+++ src/components/home/sections/Header.vue
@@ -9,10 +9,10 @@ const projectName = useProjectStore().projectName;
 <header>
     <nav class="bg-white rounded-lg border-gray-200 px-4 m-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img :src="logoSrc" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <router to="/" class="flex items-center">
+                <img :src="logoSrc" class="mr-3 h-6 sm:h-9" alt="Logo" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap">{{ projectName }}</span>
-            </a>
+            </router>
             <div class="flex items-center lg:order-2">
                 <router-link to="/auth/login" class="login-btn primary-btn text-center w-[5rem] p-2 text-sm  text-white font-medium rounded-full focus:outline-none">Login</router-link>
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-600 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
