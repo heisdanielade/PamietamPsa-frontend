f9990512f447d70e2b071a622a7fa8879ff74b22
heisdanielade <heisdanielade@gmail.com>
2025-03-18 00:42:49 +0100

    minor-changes: Logo component gradient color changed

--- src/components/others/Logo.vue
+++ src/components/others/Logo.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 
 import { useProjectStore } from '@/stores/projectStore';
@@ -8,5 +7,14 @@ const projectName = useProjectStore().projectName;
 
 <template>
     <img src="/src/assets/images/logos/webp/logo-2.webp" class="mr-1 w-6 h-6" alt="Logo" />
-    <span class="logo-font text-xl font-medium whitespace-nowrap gradient-text-1">{{ projectName }}</span>
+    <span class="logo-text text-xl font-medium whitespace-nowrap">{{ projectName }}</span>
 </template>
+
+
+<style scoped>
+.logo-text {
+    background: linear-gradient(180deg, #1b0525 0%, #591ed8 50%, #e09df3 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+</style>
