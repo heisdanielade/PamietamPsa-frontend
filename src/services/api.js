7b3d39dc26c1b4c20a610da97bcb0fd2cd697007
heisdanielade <heisdanielade@gmail.com>
2025-04-02 19:53:40 +0200

    update: updated production backend api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const DEV_API_URL = "http://localhost:8080/v1";
+// const API_URL = "http://localhost:8080/v1";
 const API_URL = "https://api-pamietampsa.up.railway.app/v1";
 
 
 const api = axios.create({
-  baseURL: DEV_API_URL,
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
