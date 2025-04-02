20d92aad8c26ffa6e1f89a275e8c58c6a7127c47
heisdanielade <heisdanielade@gmail.com>
2025-04-02 19:44:51 +0200

    update: added login redirect after registration & user email verification

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,24 +1,51 @@
 import api from "./api";
+import { useUserStore } from '@/stores/tempUser';
+
+
 
 export default {
+  // Register user 
   async register(user) {
     const response = await api.post("/auth/signup", user);
-    
-    console.log(response.data); // Test
-
     return response.data;
   },
 
+
+  // Redirect to login page after registration & email verification
+  async loginRedirect(router) {
+    const userStore = useUserStore();
+    const { email, password } = userStore;
+    // Check if userEmail and userPassword are available
+    if (!email || !password){
+      router.push('/u/login');
+    }
+    const user = {
+      email,
+      password
+    }
+    const response = await api.post("/auth/login", user);
+    const token = response.data.token;
+    localStorage.setItem("token", token); // Save token for future requests
+    router.push('/u/profile');
+  },
+
+  // Verify email after registration
+  async verifyEmail(user) {
+    const response = await api.post("/auth/verify-email", user);
+    return response.data;
+  },
+
+
+  // Login user
   async login(user) {
     const response = await api.post("/auth/login", user);
     const token = response.data.token;
-    
     if (token) {
       localStorage.setItem("token", token); // Save token for future requests
     }
-
     return response.data;
   },
+
 
   logout() {
     localStorage.removeItem("token"); // Remove token on logout
