f6e17fd60f381809674b16f38afd43eb9ebab7dc
heisdanielade <heisdanielade@gmail.com>
2025-05-06 00:24:54 +0200

    feat: added user pet card to be displayed gotten from backend endpoint and added 'no pets yet' card if user has no pets. Also added dynamic pet-card coloring

--- src/components/account/MyPets.vue
+++ src/components/account/MyPets.vue
@@ -1,60 +1,69 @@
 <script setup>
+import { onMounted, computed, ref } from "vue";
+import petService from "@/services/pet";
+
+const pets = ref([]);
+const previewPets = computed(() => pets.value.slice(0, 3));
+// const previewPets = [];
+
+const getAllPets = async () => {
+  try {
+    const allPets = await petService.allPets();
+    pets.value = allPets.data.pets;
+  } catch (error) {
+    console.error("(e) Error retrieving logged in user pet details", error);
+  }
+};
+
+onMounted(() => {
+  getAllPets();
+})
+
+
+const petColors = [
+  'bg-purple-300/30 hover:bg-purple-300/40',
+  'bg-pink-300/30 hover:bg-pink-300/40',
+  'bg-yellow-300/30 hover:bg-yellow-300/40',
+  'bg-green-300/30 hover:bg-green-300/40'
+];
 </script>
 
 
 
 <template>
 
-<section id="myPets" class="w-full flex flex-col items-start overflow-hidden px-5 mt-8 md:mt-0 md:p-3 mb-0 md:max-w-screen-md md:mx-auto lg:mx-auto lg:max-w-screen-lg">
-  <div class="w-full flex items-center justify-between mb-4">
-    <h2 class="text-2xl font-medium text-zinc-800">My Pets</h2>
-    <router-link to="/x" class="text-sm font-semibold color-dark opacity-90 hover:opacity-65 transition-all">
-      <span>View all</span>
-      <i class="ri-arrow-right-line ml-2 text-xs font-extrabold"></i>
-    </router-link>
-  </div>
+  <section id="myPets"
+    class="w-full flex flex-col items-start overflow-hidden px-5 mt-8 md:mt-0 md:p-3 mb-0 md:max-w-screen-md md:mx-auto lg:mx-auto lg:max-w-screen-lg">
 
-  <!-- Pet Cards -->
-  <div
-    v-scroll-reveal
-    class="not-shown grid gap-4 w-full grid-cols-2 sm:grid-cols-2 md:grid-cols-[repeat(auto-fit,_minmax(13rem,_1fr))] lg:gap-6"
-  >
-    <!-- Pet 1 -->
-    <router-link to="/x"
-      class="pet-card flex flex-col items-center text-center p-4 rounded-2xl border border-white/30 bg-purple-400/30 backdrop-blur-md transition-all hover:backdrop-blur-lg hover:bg-purple-400/40 md:hover:scale-105">
-      <img src="/src/assets/images/others/cat.png" alt="Bella"
-        class="w-20 h-20 rounded-full object-cover mb-4 shadow-sm border-2 border-white">
-      <h3 class="text-base font-semibold text-zinc-800">Bella</h3>
-      <p class="text-sm text-zinc-700">American Curl</p>
-      <p class="text-xs text-zinc-600 mt-1">2 years old</p>
-    </router-link>
+    <!-- Header -->
+    <div class="w-full flex items-center justify-between mb-4">
+      <h2 class="text-2xl font-medium text-zinc-800">My Pets</h2>
+      <router-link to="/x" class="text-sm font-semibold color-dark opacity-90 hover:opacity-65 transition-all">
+        <span>View all</span>
+        <i class="ri-arrow-right-line ml-2 text-xs font-extrabold"></i>
+      </router-link>
+    </div>
 
-    <!-- Pet 2 -->
-    <!-- <router-link to="/x"
-      class="pet-card flex flex-col items-center text-center p-4 rounded-2xl border border-white/30 bg-pink-300/30 backdrop-blur-md transition-all hover:backdrop-blur-lg hover:bg-pink-300/40 md:hover:scale-105">
-      <img src="/src/assets/images/backgrounds/dog-purple.png" alt="Max"
-        class="w-20 h-20 rounded-full object-cover mb-4 shadow-sm border-2 border-white">
-      <h3 class="text-base font-semibold text-zinc-800">Max</h3>
-      <p class="text-sm text-zinc-700">Siberian Husky</p>
-      <p class="text-xs text-zinc-600 mt-1">4 years old</p>
-    </router-link> -->
+    <!-- Pet -->
+    <div v-if="previewPets.length > 0" v-scroll-reveal
+      class="not-shown grid gap-4 w-full grid-cols-2 sm:grid-cols-2 md:grid-cols-[repeat(auto-fit,_minmax(13rem,_1fr))] lg:gap-6">
+      <!-- Card -->
+      <router-link to="/x" v-for="(pet, index) in previewPets" :key="pet.id"
+        :class="['pet-card', petColors[index % petColors.length], 'flex flex-col items-center text-center p-4 rounded-2xl border border-white/30 backdrop-blur-md transition-all hover:backdrop-blur-lg md:hover:scale-105']">
+        <img src="/src/assets/images/others/cat.png" :alt="pet.name"
+          class="w-20 h-20 rounded-full object-cover mb-4 shadow-sm border-2 border-white" />
+        <h3 class="text-base font-semibold text-zinc-800">{{ pet.name }}</h3>
+        <p class="text-sm text-zinc-700">{{ pet.breed }}</p>
+        <p class="text-xs text-zinc-600 mt-1">{{ pet.age }} years old</p>
+      </router-link>
+    </div>
 
-    <!-- Pet 3 -->
-    <router-link to="/x"
-      class="pet-card flex flex-col items-center text-center p-4 rounded-2xl border border-white/30 bg-yellow-300/30 backdrop-blur-md transition-all hover:backdrop-blur-lg hover:bg-yellow-300/40 md:hover:scale-105">
-      <img src="/src/assets/images/others/dog.png" alt="Haides"
-        class="w-20 h-20 rounded-full object-cover mb-4 shadow-sm border-2 border-white">
-      <h3 class="text-base font-semibold text-zinc-800">Koli</h3>
-      <p class="text-sm text-zinc-700">Beagle</p>
-      <p class="text-xs text-zinc-600 mt-1">7 years old</p>
-    </router-link>
+    <div v-else v-scroll-reveal
+      class="not-shown w-full flex flex-col items-center justify-center text-center h-28 bg-white/30 rounded-2xl border border-white/30 backdrop-blur-md shadow-inner">
+      <i class="text-zinc-500 text-xl fa-solid fa-dog"></i>
+      <p class="text-zinc-600 text-sm mt-2">No pets yet.</p>
+    </div>
 
-    <!-- More pets... -->
-  </div>
-</section>
-
-
+  </section>
 
 </template>
-
-
