e9dd9f349c1a4da398ec7a1142110a336ba4c7ba
heisdanielade <heisdanielade@gmail.com>
2025-05-19 02:32:36 +0200

    update: minor changes

--- src/components/account/Sidebar.vue
+++ src/components/account/Sidebar.vue
@@ -3,7 +3,7 @@ import authService from "@/services/auth";
 
 // handle log out
 const handleLogout = () => {
-  authService.logout(router);
+  authService.logout();
 };
 </script>
 
@@ -21,7 +21,7 @@ const handleLogout = () => {
     <!-- Nav Links -->
     <nav class="flex flex-col space-y-7 text-zinc-300">
       <!-- Dashboard -->
-      <router-link to="/x" class="group relative">
+      <router-link to="/u/profile" class="group relative">
         <i class="ri-dashboard-line text-2xl hover:text-white"></i>
         <span
           class="absolute left-12 top-1/2 -translate-y-1/2 px-2 py-1 rounded-md bg-zinc-800 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
@@ -30,7 +30,7 @@ const handleLogout = () => {
       </router-link>
 
       <!-- Pets -->
-      <router-link to="/x" class="group relative">
+      <router-link to="/u/pets" class="group relative">
         <i class="fa-solid fa-cat text-xl hover:text-white"></i>
         <span
           class="absolute left-12 top-1/2 -translate-y-1/2 px-2 py-1 rounded-md bg-zinc-800 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
@@ -39,7 +39,7 @@ const handleLogout = () => {
       </router-link>
 
       <!-- Reminders -->
-      <router-link to="/x" class="group relative">
+      <router-link to="/u/reminders" class="group relative">
         <i class="ri-notification-2-line text-2xl hover:text-white"></i>
         <span
           class="absolute left-12 top-1/2 -translate-y-1/2 px-2 py-1 rounded-md bg-zinc-800 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
@@ -48,7 +48,7 @@ const handleLogout = () => {
       </router-link>
 
       <!-- Settings -->
-      <router-link to="/x" class="group relative">
+      <router-link to="/u/settings" class="group relative">
         <i class="ri-settings-3-line text-2xl hover:text-white"></i>
         <span
           class="absolute left-12 top-1/2 -translate-y-1/2 px-2 py-1 rounded-md bg-zinc-800 text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity">
