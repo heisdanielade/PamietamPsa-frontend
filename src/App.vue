b3506942887efd4230d9ec4171cdbbc8cd1de59e
heisdanielade <heisdanielade@gmail.com>
2025-05-19 02:33:29 +0200

    update (vital): get and set user details onMount main app entry

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <script setup>
-import { ref, provide } from 'vue';
+import { ref, onBeforeMount, provide } from 'vue';
+import userService from "@/services/user";
+import { useMainUserStore } from '@/stores/mainUserDetails';
 import RequestToast from "@/components/others/RequestToast.vue";
+import Loader from "@/components/others/Loader.vue";
 
 const toastRef = ref(null);
 provide('toast', toastRef); // Make toast globally available
+const isLoading = ref(true);
 
+const userStore = useMainUserStore();
+
+const getUserDetails = async () => {
+
+  try {
+    const userDetails = await userService.userDetails();
+    // console.log("(i) User details:", userDetails.data);
+    
+    userStore.setUserDetails(userDetails.data);
+  } catch (error) {
+    console.error("(e) Error retrieving logged in user details", error);
+  } finally {
+    await new Promise(resolve => setTimeout(resolve, 1150));
+    isLoading.value = false;
+  }
+};
+
+onBeforeMount(() => {
+  getUserDetails();
+})
 </script>
 
+
+
 <template>
-
-  <router-view />
-
-  <RequestToast ref="toastRef" />
-
+  <div v-if="isLoading">
+    <Loader/>
+  </div>
+  <div v-else>
+    <router-view />
+    <RequestToast ref="toastRef" />
+  </div>
 </template>
-
-<style scoped></style>
