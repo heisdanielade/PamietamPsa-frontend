00e3d58153a18b6df5eead9e1cd9605c091ad986
heisdanielade <heisdanielade@gmail.com>
2025-05-28 23:44:42 +0200

    update: userStore data for user's name in greeting component

--- src/components/account/Greeting.vue
+++ src/components/account/Greeting.vue
@@ -1,7 +1,16 @@
 
 <script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import { useMainUserStore } from '@/stores/mainUserDetails';
+
+const userStore = useMainUserStore();
+const router = useRoute();
+const isHomePage = computed(() => router.name === "MyProfile");
+const isSettingsPage = computed(() => router.name === "Settings");
+
 const user = {
-    name: "",
+    name: userStore.name || "",
 }
 </script>
 
@@ -10,13 +19,19 @@ const user = {
 <template>
     <div class="w-full overflow-hidden px-5 md:p-3 mb-0 xmd:mb-10 md:max-w-screen-md md:mx-auto lg:mx-auto lg:max-w-screen-lg">
         <h1 class="font-medium text-2xl md:text-2xl gradient-text-3">
-            Hi there, <span v-if="user.name" class="">{{ user.name }}</span>
+            Hi there, <span class="">{{ user.name }}</span>
         </h1>
         <p class="font-semibold color-dark opacity-60">
-            Local pet care services for you.
+           <span v-if="isHomePage" class="">
+             Local pet care services for you.
+           </span>
+
+           <span v-else class="">
+            Tweak your account settings
+           </span>
         </p>
 
-        <router-link to="/x" v-scroll-reveal class="not-shown promo-card mt-3 w-full lg:w-7/12 flex items-center justify-between gap-4 flex-shrink-0 p-5 rounded-2xl bg-gradient-to-r from-purple-500 via-pink-400 to-fuchsia-400 backdrop-blur-md border border-white/30 transition hover:backdrop-blur-lg hover:brightness-105">
+        <router-link v-if="isHomePage" to="/x" v-scroll-reveal class="not-shown promo-card mt-3 w-full lg:w-7/12 flex items-center justify-between gap-4 flex-shrink-0 p-5 rounded-2xl bg-gradient-to-r from-purple-500 via-pink-400 to-fuchsia-400 backdrop-blur-md border border-white/30 transition hover:backdrop-blur-lg hover:brightness-105">
             <div class="flex flex-col">
                 <h2 class="text-lg font-bold text-white mb-1">Pet Lover Zone!</h2>
                 <p class="text-sm text-white/90 mb-5">Get more valuable tips.</p>
